cecefb8f9f56f0bdae9007ae8d709a8bbb17571b
karthe360 <karthegetu@gmail.com>
2024-12-05 12:54:58 +0530

    lp_yr

--- leap_year.py
+++ leap_year.py
@@ -1,7 +1,7 @@
 lp_y=int(input("enter year:"))
 
 
-if i%4==0 and i%100!=0 or i%400==0:
+if lp_y%4==0 and lp_y%100!=0 or lp_y%400==0:
         print(f"leap year is{lp_y}")
 else:
     print(f"not leap year{lp_y}")
