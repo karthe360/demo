0b21a14ca080176bc3763ef0f1cbc3f1e8db85f3
karthe360 <karthegetu@gmail.com>
2024-12-10 15:27:47 +0530

    modify

--- odd_even.py
+++ odd_even.py
@@ -2,7 +2,11 @@
 
 x=int(input("enter the number:"))
 
+print("program code")
+
 if x%2==0:
     print(f"The User Input no :{x} is even ")
 else:
     print(f"The User input no :{x} is odd")
+
+print("program_success")
